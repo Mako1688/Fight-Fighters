bcaa3a933c781bfc420b5a36428323e423994506
Marco Ogaz-Vega <marcoogazvega@hotmail.com>
2024-02-22 14:45:42 -0800

    completed character select

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -19,6 +19,10 @@ class Load extends Phaser.Scene {
         this.load.image('usa flag', './assets/Sprites/America Flag.png')
         this.load.image('characters_select', './assets/Sprites/Characters.png')
         this.load.image('title_background', './assets/Sprites/title.png')
+        this.load.image('Karate Portrait', './assets/Sprites/Karate Portrait.png')
+        this.load.image('Karate Name', './assets/Sprites/Karate Name.png')
+        this.load.image('Rumble Portrait', './assets/Sprites/Rumble Portrait.png')
+        this.load.image('Rumble Name', './assets/Sprites/Rumble Name.png')
 
         //load spritesheets
         this.load.spritesheet('p1_cursor_sheet', './assets/SpriteSheets/p1 cursor sheet.png', {
--- src/Scenes/Select.js
+++ src/Scenes/Select.js
@@ -21,6 +21,16 @@ class Select extends Phaser.Scene {
         this.p1cursor = this.add.sprite(this.select.x - 35, this.select.y, 'p1_cursor_sheet', 0)
         this.p2cursor = this.add.sprite(this.select.x - 35, this.select.y, 'p2_cursor_sheet', 0)
 
+        //set player 1 portrait
+        this.p1portrait = this.add.sprite(game.config.width/ 5, game.config.height / 3 * 2, 'Rumble Portrait')
+        this.p1name = this.add.sprite(this.p1portrait.x, this.p1portrait.y - 120, 'Rumble Name').setScale(5)
+        this.p1flag = this.add.sprite(this.p1name.x, this.p1name.y - 100, 'usa flag')
+        
+        //set player 2 portrait
+        this.p2portrait = this.add.sprite(game.config.width/ 5 * 4, game.config.height / 3 * 2, 'Rumble Portrait').setFlipX(true)
+        this.p2name = this.add.sprite(this.p2portrait.x, this.p2portrait.y - 120, 'Rumble Name').setScale(5)
+        this.p2flag = this.add.sprite(this.p2name.x, this.p2name.y - 100, 'usa flag')
+
         //define p1 keys
         p1Left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
         p1Right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
@@ -50,41 +60,28 @@ class Select extends Phaser.Scene {
         this.p1cursor.anims.play('p1_cursor_flash', true)
         this.p2cursor.anims.play('p2_cursor_flash', true)
 
-        // if(p1Left.isDown && this.p1Rumble == true) {
-        //     this.p1Karate = true
-        //     this.p1Rumble = false
-        //     this.p1cursor.x = this.select.x + 35
-        // } else if(p1Left.isDown && this.p1Karate == true) {
-        //     this.p1Rumble = true
-        //     this.p1Karate = false
-        //     this.p1cursor.x = this.select.x - 35
-        // } else if(p1Right.isDown && this.p1Rumble == true) {
-        //     this.p1Karate = true
-        //     this.p1Rumble = false
-        //     this.p1cursor.x = this.select.x + 35
-        // } else if(p1Right.isDown && this.p1Karate == true) {
-        //     this.p1Rumble = true
-        //     this.p1Karate = false
-        //     this.p1cursor.x = this.select.x - 35
-        // }
+        //set p1 character portrait bools
+        if(this.p1Rumble == true && this.p1Karate == false) {
+            this.p1portrait.setTexture('Rumble Portrait').setFlipX(false)
+            this.p1name.setTexture('Rumble Name').setScale(5)
+            this.p1flag.setTexture('usa flag')
+        } else if (this.p1Karate == true && this.p1Rumble == false) {
+            this.p1portrait.setTexture('Karate Portrait').setFlipX(true)
+            this.p1name.setTexture('Karate Name').setScale(5)
+            this.p1flag.setTexture('jpn flag')
+        }
 
-        // if(p2Left.isDown && this.p2Rumble == true) {
-        //     this.p2Karate = true
-        //     this.p2Rumble = false
-        //     this.p2cursor.x = this.select.x + 35
-        // } else if(p2Left.isDown && this.p2Karate == true) {
-        //     this.p2Rumble = true
-        //     this.p2Karate = false
-        //     this.p2cursor.x = this.select.x - 35
-        // } else if(p2Right.isDown && this.p2Rumble == true) {
-        //     this.p2Karate = true
-        //     this.p2Rumble = false
-        //     this.p2cursor.x = this.select.x + 35
-        // } else if(p2Right.isDown && this.p2Karate == true) {
-        //     this.p2Rumble = true
-        //     this.p2Karate = false
-        //     this.p2cursor.x = this.select.x - 35
-        // }
+        //set p2 character bools
+        if(this.p2Rumble == true && this.p2Karate == false) {
+            this.p2portrait.setTexture('Rumble Portrait').setFlipX(true)
+            this.p2name.setTexture('Rumble Name').setScale(5)
+            this.p2flag.setTexture('usa flag')
+        } else if (this.p2Karate == true && this.p2Rumble == false) {
+            this.p2portrait.setTexture('Karate Portrait').setFlipX(false)
+            this.p2name.setTexture('Karate Name').setScale(5)
+            this.p2flag.setTexture('jpn flag')
+        }
+
         
         // Handle player 1 left key on rumble
         if (p1Left.isDown && this.p1LeftReleased && this.p1Rumble == true) {
